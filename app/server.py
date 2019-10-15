259df7247adb3096ca53b38a66082fcb85f6e2e4
tahercoolguy <tahercoolguy@gmail.com>
2019-10-15 09:02:55 +0300

    Add files via upload

--- app/server.py
+++ app/server.py
@@ -70,6 +70,30 @@ learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
 loop.close()
 
 
+def sort_contours(cnts, method="left-to-right"):
+	# initialize the reverse flag and sort index
+	reverse = False
+	i = 0
+ 
+	# handle if we need to sort in reverse
+	if method == "right-to-left" or method == "bottom-to-top":
+		reverse = True
+ 
+	# handle if we are sorting against the y-coordinate rather than
+	# the x-coordinate of the bounding box
+	if method == "top-to-bottom" or method == "bottom-to-top":
+		i = 1
+ 
+	# construct the list of bounding boxes and sort them from top to
+	# bottom
+	boundingBoxes = [cv2.boundingRect(c) for c in cnts]
+	(cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),
+		key=lambda b:b[1][i], reverse=reverse))
+ 
+	# return the list of sorted contours and bounding boxes
+	return (cnts, boundingBoxes)
+
+
 def rotate_image(mat, angle):
     """
     Rotates an image (angle in degrees) and expands image to avoid cropping
@@ -120,9 +144,9 @@ def predict_append():
 
     # Get rectangles contains each contour
     # rects = [cv2.boundingRect(ctr) for ctr in hier]
+    cntrs,bb=sort_contours(ctrs)
 
-
-    for (i, c) in enumerate(ctrs):
+    for (i, c) in enumerate(cntrs):
         (x, y, w, h) = cv2.boundingRect(c)
     
 
