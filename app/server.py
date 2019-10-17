7cf82d437ff7546f72bce946ef18b013f2bd8676
tahercoolguy <tahercoolguy@gmail.com>
2019-10-17 13:13:47 +0300

    Add files via upload

--- app/server.py
+++ app/server.py
@@ -69,6 +69,27 @@ tasks = [asyncio.ensure_future(setup_learner())]
 learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
 loop.close()
 
+def removeShadow(img):
+
+  rgb_planes = cv2.split(img)
+
+  result_planes = []
+  result_norm_planes = []
+  for plane in rgb_planes:
+      dilated_img = cv2.dilate(plane, np.ones((7,7), np.uint8))
+      bg_img = cv2.medianBlur(dilated_img, 21)
+      diff_img = 255 - cv2.absdiff(plane, bg_img)
+      norm_img = cv2.normalize(diff_img,None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
+      result_planes.append(diff_img)
+      result_norm_planes.append(norm_img)
+
+  result = cv2.merge(result_planes)
+  result_norm = cv2.merge(result_norm_planes)
+
+  cv2.imwrite('shadows_out.png', result)
+  cv2.imwrite('shadows_out_norm.png', result_norm)
+  return result_norm
+
 
 def sort_contours(cnts, method="left-to-right"):
 	# initialize the reverse flag and sort index
@@ -124,7 +145,7 @@ def rotate_image(mat, angle):
 predictions=[]
 def predict_append():
     im = cv2.imread("new.jpg")
-
+    im = removeShadow(im)
     im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     im_gray = cv2.GaussianBlur(im_gray, (5, 5), 0)
 
